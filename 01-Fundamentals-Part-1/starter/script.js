df2d82d949074388c51feea50ad8538bc4fcf9f2
vanessadubouzet <vanessadubouzet@gmail.com>
2024-05-31 15:53:34 -0700

    Completed Coding Challenge #1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,19 @@
+//CODING CHALLENGE #1
+
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark);
+console.log(BMIJohn);
+console.log(markHigherBMI);
+
+// CODING CHALLENGE #2
+
