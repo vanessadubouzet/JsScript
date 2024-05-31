1516d124c808effb93c20b224e14339ca7582743
vanessadubouzet <vanessadubouzet@gmail.com>
2024-05-31 15:57:05 -0700

    Completed Coding Challenge #2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -17,3 +17,10 @@ console.log(markHigherBMI);
 
 // CODING CHALLENGE #2
 
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+} else {
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`)
+}
+
+//
