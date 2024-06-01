c2237ff0c3e1deafb555158ee94043bde2e90df2
vanessadubouzet <vanessadubouzet@gmail.com>
2024-05-31 19:24:32 -0700

    Completed Coding Challenge #4

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -35,3 +35,11 @@ if (scoreDolphins > scoreKoalas) {
 } else {
     console.log ("Both win the trophy");
 }
+
+// CODING CHALLENGE #4
+
+const bill = 275;
+
+const tip = (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.2;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
