4bae63c261acec0c54d0568acd1b8c7c7b46e424
vanessadubouzet <vanessadubouzet@gmail.com>
2024-07-09 13:44:04 -0700

    Update script.js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,32 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// // SLICE (shallow copy of an array)
+// console.log(arr.slice(2));
+// console.log(arr.slice(2,4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice());
+// console.log([...arr]);
+
+// // SPLICE (mutates the original array)
+// // console.log(arr.splice(2));
+// arr.splice(-1);
+// console.log(arr);
+
+// // REVERSE (mutates the original array)
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// let arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+// console.log(arr2);
+
+// // CONCAT (doesnt mutate the original array)
+// const letters = arr.concat(arr2);
+// console.log(letters);
+
+// // JOIN 
+// console.log(letters.join(' - '));
+
+
