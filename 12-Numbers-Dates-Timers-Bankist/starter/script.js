5d12b5870f3e99cb35631093e9f77800ddd4e566
vanessadubouzet <vanessadubouzet@gmail.com>
2024-07-23 19:30:59 -0700

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -380,3 +380,37 @@ btnSort.addEventListener('click', function (e) {
 // // Divisions
 // console.log( 10n / 3n);
 
+// Create a date
+// const now = new Date();
+// console.log(now);
+
+// console.log(new Date('Jul 23 2024 19:21:24'));
+// console.log(new Date('December 24, 2015'));
+// console.log(new Date(account1.movementsDates[0]));
+
+// console.log(new Date(2037, 10, 19, 15, 23, 5));
+// console.log(new Date(2037, 10, 31));
+
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
+
+
+
