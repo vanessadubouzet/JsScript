491a2ba66c03dcc9df2b2c04702d279f2b9721cd
vanessadubouzet <vanessadubouzet@gmail.com>
2024-07-24 14:56:28 -0700

    page navigation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,13 +1,14 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
+///////////////////////////////////////
+// Modal window
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove('hidden');
@@ -30,41 +31,8 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-////////////////////////////////////////////////
-//// LECTURE
-
-// Event Propagation / Bubbling
-// rgb(255,255,255);
-const randomInt = (min,max) => Math.floor(Math.random() * (max - min + 1) + min);
-const randomColor = () => `rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`
-console.log(randomInt(0,255));
-
-
-document.querySelector('.nav__link').addEventListener('click', function(e){
-  this.style.backgroundColor = randomColor();
-  console.log('LINK', e.target, e.currentTarget);
-
-  // Stop propagation (not a good idea in practice)
-  // e.stopPropagation();
-});
-
-document.querySelector('.nav__links').addEventListener('click', function(e){
-  this.style.backgroundColor = randomColor(); 
-  console.log('CONTAINER', e.target, e.currentTarget);
-
-});
-
-document.querySelector('.nav').addEventListener('click', function(e){
-  this.style.backgroundColor = randomColor(); 
-  console.log('NAV', e.target, e.currentTarget);
-}, true);
-
-
-
-// Smooth Scrolling
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
+///////////////////////////////////////
+// Button Scrolling
 btnScrollTo.addEventListener('click', function(e){
     const s1coords = section1.getBoundingClientRect();
     // console.log(s1coords);
@@ -89,6 +57,67 @@ btnScrollTo.addEventListener('click', function(e){
         behavior: 'smooth'
     });
 });
+
+///////////////////////////////////////
+// Page Navigation
+// document.querySelectorAll('.nav__link').forEach(function(el) {
+//     el.addEventListener('click', function(e) {
+//         e.preventDefault();
+//         const id = this.getAttribute('href');
+//         console.log(id);
+//         document.querySelector(id).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     })
+// })
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+    e.preventDefault();
+    // Matching strategy
+    if(e.target.classList.contains('nav__link')) {
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+});
+
+
+
+////////////////////////////////////////////////
+//// LECTURE
+
+// DOM Traversing
+
+// Event Propagation / Bubbling
+// rgb(255,255,255);
+// const randomInt = (min,max) => Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () => `rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`
+// console.log(randomInt(0,255));
+
+
+// document.querySelector('.nav__link').addEventListener('click', function(e){
+//   this.style.backgroundColor = randomColor();
+//   console.log('LINK', e.target, e.currentTarget);
+
+//   // Stop propagation (not a good idea in practice)
+//   // e.stopPropagation();
+// });
+
+// document.querySelector('.nav__links').addEventListener('click', function(e){
+//   this.style.backgroundColor = randomColor(); 
+//   console.log('CONTAINER', e.target, e.currentTarget);
+
+// });
+
+// document.querySelector('.nav').addEventListener('click', function(e){
+//   this.style.backgroundColor = randomColor(); 
+//   console.log('NAV', e.target, e.currentTarget);
+// });
+
 
 // const h1 = document.querySelector('h1');
 
