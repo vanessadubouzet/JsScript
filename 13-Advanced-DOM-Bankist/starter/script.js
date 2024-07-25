763ac788a5ec993fc4419c3c975de172b287f225
vanessadubouzet <vanessadubouzet@gmail.com>
2024-07-24 18:06:58 -0700

    Update script.js

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -129,6 +129,37 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
 
 ////////////////////////////////////////////////
+//// Sticky Navigation
+// const initialCoords = section1.getBoundingClientRect();
+// window.addEventListener('scroll', function() {
+//     if(window.scrollY > initialCoords.top) 
+//         nav.classList.add('sticky');
+//     else 
+//         nav.classList.remove('sticky');
+
+// })
+
+// Sticky navigation: Intersection Observer API
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function(entries) {
+    const [entry] = entries;
+    if(!entry.isIntersecting)
+        nav.classList.add('sticky');
+    else 
+        nav.classList.remove('sticky');
+}
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+
+
+////////////////////////////////////////////////
 //// LECTURE
 
 // DOM Traversing
